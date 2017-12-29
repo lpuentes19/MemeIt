14359a409da7638e788a4be003ebfbcd4c87b67a
lpuentes19 <luey.puentes@gmail.com>
2017-12-29 15:59:15 -0700

    Have setup the view in Interface Builder and also created the VC for the view and have rigged the outlets and IBAction to the VC. It now conforms to the UIIMagePickerControllerDelegate.

---
+++ MemeIt/MemeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MemeViewController.swift
+//  MemeIt
+//
+//  Created by Luis Puentes on 12/29/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+}
