67285f9831c78eeaa5cb7b5a90ee40edca92c86b
lpuentes19 <luey.puentes@gmail.com>
2017-12-29 19:13:29 -0700

    Added all appropriate textFieldDelegate methods and logic within each method.

--- MemeIt/MemeViewController.swift
+++ MemeIt/MemeViewController.swift
@@ -8,19 +8,67 @@
 
 import UIKit
 
-class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setMemeTextAttributesAndDelegate()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    func setMemeTextAttributesAndDelegate() {
+        
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+        let memeTextAttributes: [String: Any] = [
+            NSAttributedStringKey.strokeColor.rawValue: UIColor.white,
+            NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+            NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue", size: 25)!,
+            NSAttributedStringKey.strokeWidth.rawValue: 10]
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if topTextField == textField {
+            topTextField.text = ""
+        }
+        
+        if bottomTextField == textField {
+            bottomTextField.text = ""
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    
+        if topTextField.text == "" {
+            topTextField.text = "TOP"
+        }
+        
+        if bottomTextField.text == "" {
+            bottomTextField.text = "BOTTOM"
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -49,6 +97,4 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         imagePicker.sourceType = .photoLibrary
         self.present(imagePicker, animated: true, completion: nil)
     }
-    
-    
 }
