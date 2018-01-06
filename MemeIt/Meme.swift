3218605a15eae7d69836942eae28f2de3584d57f
lpuentes19 <luey.puentes@gmail.com>
2018-01-06 10:08:45 -0700

    Added a Meme model object that takes in a top and bottom text also an image.

---
+++ MemeIt/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeIt
+//
+//  Created by Luis Puentes on 1/6/18.
+//  Copyright © 2018 LuisPuentes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var image: UIImage
+}
